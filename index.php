3d353fcc1274b49ab432a640426f99edb956cafe
amedeopro <amedeopro@me.com>
2019-05-15 23:25:07 +0200

    student class

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require 'Student.php';
 
+//Amedeo Pro
 $studenteAmedeo = new Student();
 
 $studenteAmedeo->firstname = 'Amedeo';
@@ -12,6 +13,19 @@ $studenteAmedeo->grades = ['6','5','4'];
 echo 'Voto medio dello studente '.$studenteAmedeo->firstname.' '.$studenteAmedeo->lastname.': '.$studenteAmedeo->averageGrades($studenteAmedeo->grades).'<br>';
 
 echo 'Età dello studente '.$studenteAmedeo->firstname.' '.$studenteAmedeo->lastname.': '.$studenteAmedeo->age($studenteAmedeo->birthdate).'<br>';
+
+
+//Mario Rossi
+$studenteMario = new Student();
+
+$studenteMario->firstname = 'Mario';
+$studenteMario->lastname = 'Rossi';
+$studenteMario->birthdate = '27/02/1979';
+$studenteMario->grades = ['7','6','8'];
+
+echo 'Voto medio dello studente '.$studenteMario->firstname.' '.$studenteMario->lastname.': '.$studenteMario->averageGrades($studenteMario->grades).'<br>';
+
+echo 'Età dello studente '.$studenteMario->firstname.' '.$studenteMario->lastname.': '.$studenteMario->age($studenteMario->birthdate).'<br>';
 
 
 
